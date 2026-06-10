32ed555646fcf13095ee607b206798fd08f41f57
Vickouma77 <Vickprestone20@gmail.com>
2026-06-10 18:04:43 +0300

    fix: update main entry point to src/index.js and add video streaming functionality

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+app.get('/video', async (req, res) => {
+    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+    const stats = await fs.promises.stat(videoPath);
+
+    res.writeHead(200, {
+        "content-length": stats.size,
+        "content-type": "video/mp4",
+    });
+
+    fs.createReadStream(videoPath).pipe(res);
+});
+
+app.listen(port);
